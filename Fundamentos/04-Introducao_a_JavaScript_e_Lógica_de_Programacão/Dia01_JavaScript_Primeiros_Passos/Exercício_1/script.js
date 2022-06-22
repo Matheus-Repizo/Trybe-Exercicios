7123c055fa46e5d0e8d6835c0ba120a0f0294f93
Matheus Repizo <matheusdprepizo@gmail.com>
2022-06-22 10:00:20 -0300

    Incluindo Variáveis e Constantes

---
+++ Fundamentos/04-Introducao_a_JavaScript_e_Lógica_de_Programacão/Dia01_JavaScript_Primeiros_Passos/Exercício_1/script.js
@@ -0,0 +1,8 @@
+const myName = "Matheus Repizo";
+const birthCity = "São Paulo";
+let birtYear = 1994;
+console.log( myName,birthCity,birtYear);
+birtYear= 2030;
+console.log(birtYear)
+birthCity = "Santa Cruz do Rio Pardo";
+console.log(birthCity);
