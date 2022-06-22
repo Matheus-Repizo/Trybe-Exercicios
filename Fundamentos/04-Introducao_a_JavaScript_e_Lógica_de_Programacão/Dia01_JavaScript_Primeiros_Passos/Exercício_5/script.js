50de629e8b0293a2d5cbaea1fd2cfbb897142404
Matheus Repizo <matheusdprepizo@gmail.com>
2022-06-22 11:52:21 -0300

    Executando Switch Case

---
+++ Fundamentos/04-Introducao_a_JavaScript_e_Lógica_de_Programacão/Dia01_JavaScript_Primeiros_Passos/Exercício_5/script.js
@@ -0,0 +1,17 @@
+let statusSelection  = "Xablau"
+switch(statusSelection){
+    case "Aprovada(o)":
+    console.log("Aprovada(o)");
+    break; 
+
+    case "Reprovada(o)":
+    console.log("Reprovada(o)");
+    break;
+
+    case "Lista":
+    console.log("Lista");
+    break;
+
+    default:
+        console.log("Não se Aplica")
+}
