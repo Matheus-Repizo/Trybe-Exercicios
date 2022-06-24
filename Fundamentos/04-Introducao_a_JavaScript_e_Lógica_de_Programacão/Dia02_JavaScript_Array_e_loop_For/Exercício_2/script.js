c0b475504bd2443b4ddbc9d14d1b2be645f89060
Matheus Repizo <matheusdprepizo@gmail.com>
2022-06-24 10:20:03 -0300

    Utilizando For e For of

---
+++ Fundamentos/04-Introducao_a_JavaScript_e_Lógica_de_Programacão/Dia02_JavaScript_Array_e_loop_For/Exercício_2/script.js
@@ -0,0 +1,16 @@
+//Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let i = 0; i<groceryList.length;i+=1){
+    console.log(groceryList[i])
+};
+
+//Exercício 1
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let name of names){
+    console.log(name);
+}
