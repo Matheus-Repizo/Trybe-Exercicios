e597d0c32b1214f402849350121847c8275544cd
Matheus Repizo <matheusdprepizo@gmail.com>
2022-06-30 10:34:56 -0300

    Manipulando elementos atraves do DOM

--- Fundamentos/05-JavaScript:DOM,Eventos_e_Web_Storage/Dia02_JavaScript-Trabalhando_com_elementos/Exercício_1/script.js
+++ Fundamentos/05-JavaScript:DOM,Eventos_e_Web_Storage/Dia02_JavaScript-Trabalhando_com_elementos/Exercício_1/script.js
@@ -3,25 +3,25 @@
 //Acesse o elemento elementoOndeVoceEsta.
 const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
 
-//Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele. 
+//Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const father = elementoOndeVoceEsta.parentElement;
 father.style.color = "green";
 
-//Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso? 
+//Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 const firstElementChildChild = elementoOndeVoceEsta.firstElementChild;
-firstElementChildChild.innerText = "Filhotinho"
+firstElementChildChild.innerText = "Filhotinho";
 
 //Acesse o primeiroFilho a partir de pai.
 const first_Child = father.firstElementChild;
 
-//Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta. 
+//Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
 const first_Child2 = elementoOndeVoceEsta.previousElementSibling;
 
 //Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
- const text = elementoOndeVoceEsta.nextSibling;
+const text = elementoOndeVoceEsta.nextSibling;
 
- //Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta. 
- const terceiroFilho = elementoOndeVoceEsta.nextElementSibling
+//Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
 
- // 8. Agora acesse o terceiroFilho a partir de pai.
- const terceiroFilho2 = father.lastElementChild.previousElementSibling;
+//Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilho2 = father.lastElementChild.previousElementSibling;
